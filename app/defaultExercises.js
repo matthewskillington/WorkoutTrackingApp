888a00a64bf65ffbfaa7c48c5cd965084a9e4a19
matthewskillington <matthew.skillington@ideagenplc.com>
2021-02-08 21:00:44 +0000

    Add reps property

--- app/defaultExercises.js
+++ app/defaultExercises.js
@@ -1,45 +1,52 @@
 const ExerciseList = [
-    {
-      id: 1,
-      name: 'Dumbbell Curl',
-      lastPerformed: ""
-  
-    },
-    {
-      id: 2,
-      name: 'Dumbbell Row',
-      lastPerformed: ""
-    },
-    {
-      id: 3,
-      name: 'Front Elevations',
-      lastPerformed: ""
-    },
-    {
-      id: 4,
-      name: 'Side Elevations',
-      lastPerformed: ""
-    },
-    {
-      id: 5,
-      name: 'Dumbbell Shrugs',
-      lastPerformed: ""
-    },
-    {
-      id: 6,
-      name: 'Dumbbell Pull Over',
-      lastPerformed: ""
-    },
-    {
-      id: 7,
-      name: 'Tricep Extensions',
-      lastPerformed: ""
-    },
-    {
-      id: 8,
-      name: 'Wrist Extensions',
-      lastPerformed: ""
-    }
+  {
+    id: 1,
+    name: 'Dumbbell Curl',
+    lastPerformed: '',
+    reps: 0,
+  },
+  {
+    id: 2,
+    name: 'Dumbbell Row',
+    lastPerformed: '',
+    reps: 0,
+  },
+  {
+    id: 3,
+    name: 'Front Elevations',
+    lastPerformed: '',
+    reps: 0,
+  },
+  {
+    id: 4,
+    name: 'Side Elevations',
+    lastPerformed: '',
+    reps: 0,
+  },
+  {
+    id: 5,
+    name: 'Dumbbell Shrugs',
+    lastPerformed: '',
+    reps: 0,
+  },
+  {
+    id: 6,
+    name: 'Dumbbell Pull Over',
+    lastPerformed: '',
+    reps: 0,
+  },
+  {
+    id: 7,
+    name: 'Tricep Extensions',
+    lastPerformed: '',
+    reps: 0,
+  },
+  {
+    id: 8,
+    name: 'Wrist Extensions',
+    lastPerformed: '',
+    reps: 0,
+  },
 ];
-  
+
 export default ExerciseList;
