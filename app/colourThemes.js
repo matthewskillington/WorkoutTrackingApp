518d5132affba1403528d4b80bd8eb979a86ed31
matthewskillington <matthew.skillington@ideagenplc.com>
2021-02-22 21:13:35 +0000

    Add colourThemes which i missed

---
+++ app/colourThemes.js
@@ -0,0 +1,15 @@
+const LightTheme = {
+  MasterGrey100: '#FFF',
+  TitleText: '#1E1C26',
+  SecondaryText: '#595763',
+  Success: '#24ab75',
+};
+
+const DarkTheme = {
+  MasterGrey100: '#3d3d3d',
+  TitleText: '#F6F6F6',
+  SecondaryText: '#bababa',
+  Success: '#24ab75',
+};
+
+export {LightTheme, DarkTheme};
