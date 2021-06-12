ecae2d8a4d779e6b0f12665c05d8c03bb625536b
Matthew Skillington <matthewskillington14@gmail.com>
2021-06-12 11:34:27 +0100

    add dark theme to react navigation

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
 import HomeList from './components/HomeList/HomeList';
 import MetricsPage from './components/MetricsPage/MetricsPage';
@@ -22,8 +22,21 @@ const App: React.FC = () => {
     }
   });
 
+  const NavigationDarkTheme = {
+    dark: true,
+    colors: {
+      primary: 'rgb(255, 45, 85)',
+      background: 'rgb(61, 61, 61)',
+      card: 'rgb(61, 61, 61)',
+      text: 'rgb(255, 255, 255)',
+      border: 'rgb(199, 199, 204)',
+      notification: 'rgb(255, 69, 58)',
+    },
+  };
+
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      theme={useColorScheme() === 'dark' ? NavigationDarkTheme : DefaultTheme}>
       <ThemeContext.Provider
         value={theme}>
         <SafeAreaView style={styles.safeAreaStyles}>
